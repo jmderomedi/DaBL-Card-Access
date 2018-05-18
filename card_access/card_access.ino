992950cb054ea6c93eab703e2c61d929966e3c74
Kristof Aldenderfer <kristof@american.edu>
2018-05-18 14:32:31 -0400

    begin integration of time

--- card_access/card_access.ino
+++ card_access/card_access.ino
@@ -1,5 +1,3 @@
-// this inserts information - but not the correct information.
-
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_PN532.h>
@@ -7,6 +5,8 @@
 #include <WiFiEsp.h>
 #include <MySQL_Connection.h>
 #include <MySQL_Cursor.h>
+#include <TimeLib.h> 
+#include <WiFiEspUdp.h>
 
 // pn532
 const uint8_t pn532_SCK  = 13;
@@ -23,7 +23,7 @@ String windows_password = "";    // Windows password
 // esp8266
 WiFiEspClient client;
 byte esp8266_mac[6];
-char wifi_ssid[] = "";                  // Network Name
+char wifi_ssid[] = "AUGuest-ByRCN";                  // Network Name
 char wifi_password[] = "";       // Network Password
 
 // server
@@ -32,6 +32,14 @@ IPAddress server_addr(192, 168, 0, 101);    // MySQL server IP
 MySQL_Connection conn((Client *)&client);
 char mysql_user[] = "";                 // MySQL user
 char mysql_password[] = "";                // MySQL password
+
+// time stuff
+char timeServer[] = "time.nist.gov";  // NTP server
+unsigned int localPort = 2390;        // local port to listen for UDP packets
+const int NTP_PACKET_SIZE = 48;  // NTP timestamp is in the first 48 bytes of the message
+const int UDP_TIMEOUT = 2000;    // timeout in miliseconds to wait for an UDP packet to arrive
+byte packetBuffer[NTP_PACKET_SIZE]; // buffer to hold incoming and outgoing packets
+WiFiEspUDP Udp;
 
 // search and general
 // possibilities: master, typea, stratasys, bantam, pls475, jaguarv
@@ -50,6 +58,8 @@ void setup() {
     while (!Serial);
     Serial1.begin(115200);
     WiFi.init(&Serial1);
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(wifi_ssid);
     WiFi.begin(wifi_ssid, wifi_password);
     elapsedMillis wifi_timer = 0;
     while (WiFi.status() != WL_CONNECTED && wifi_timer<10000);
@@ -59,6 +69,13 @@ void setup() {
     }
     WiFi.macAddress(esp8266_mac);
     print_network();
+    Udp.begin(localPort);
+    while (1) {
+        if (WiFi.status() == WL_CONNECTED) {
+            get_time();
+            delay(10000);
+        }
+    }
 }
 
 void loop() {
@@ -178,7 +195,7 @@ bool connect_to_db() {
     }
 }
 
-// todo: flesh this out
+// todo: better returning of information
 bool log_access(String dbnametable, String uid, String name, String date, String time) {
     char query[128];
 
@@ -239,6 +256,86 @@ String print_hex(const byte * data, const uint32_t numBytes) {
     Serial.println();
     str.toUpperCase();
     return str;
+}
+
+void get_time() {
+    sendNTPpacket(timeServer); // send an NTP packet to a time server
+  
+  // wait for a reply for UDP_TIMEOUT miliseconds
+  unsigned long startMs = millis();
+  while (!Udp.available() && (millis() - startMs) < UDP_TIMEOUT) {}
+
+  Serial.println(Udp.parsePacket());
+  if (Udp.parsePacket()) {
+    Serial.println("packet received");
+    // We've received a packet, read the data from it into the buffer
+    Udp.read(packetBuffer, NTP_PACKET_SIZE);
+
+    // the timestamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, esxtract the two words:
+
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+    Serial.print("Seconds since Jan 1 1900 = ");
+    Serial.println(secsSince1900);
+
+    // now convert NTP time into everyday time:
+    Serial.print("Unix time = ");
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    const unsigned long seventyYears = 2208988800UL;
+    // subtract seventy years:
+    unsigned long epoch = secsSince1900 - seventyYears;
+    // print Unix time:
+    Serial.println(epoch);
+
+
+    // print the hour, minute and second:
+    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
+    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
+    Serial.print(':');
+    if (((epoch % 3600) / 60) < 10) {
+      // In the first 10 minutes of each hour, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+    Serial.print(':');
+    if ((epoch % 60) < 10) {
+      // In the first 10 seconds of each minute, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.println(epoch % 60); // print the second
+  }
+  // wait ten seconds before asking for the time again
+}
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(char *ntpSrv)
+{
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(ntpSrv, 123); //NTP requests are to port 123
+
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+
+  Udp.endPacket();
 }
 
 // ====================================================================================
